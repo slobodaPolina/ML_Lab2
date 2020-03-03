fb77a5f9a055a88f1b58362d1b1f11c77c0197ed
Dmitrii <1@1.1>
2020-03-03 22:56:51 +0300

    Fix

--- iteration_plotter.py
+++ iteration_plotter.py
@@ -13,9 +13,10 @@ for max_iterations in range(num_iterations):
     #train_loss, test_loss, _ = model({'learning_rate': parameters.gradient_descent['learning_rate'],
     #                               'regularization_strength': parameters.gradient_descent['regularization_strength'],
     #                               'num_iterations': max_iterations + 1})
+    print('Max iterations: {}: '.format(max_iterations + 1))
 
     train_loss, test_loss, _ = model({'mutation_amplitude': parameters.genetic['mutation_amplitude'], 'regularization_strength': parameters.genetic['regularization_strength'],
-                                                          'num_chromosomes': parameters.genetic['num_chromosomes'], 'num_generations': num_iterations + 1,
+                                                          'num_chromosomes': parameters.genetic['num_chromosomes'], 'num_generations': max_iterations + 1,
                                                           'parent_proportion': parameters.genetic['parent_proportion'], 'random_gene_amplitude': parameters.genetic['random_gene_amplitude']})
 
     best_losses.append(min(test_loss))
